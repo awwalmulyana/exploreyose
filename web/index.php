adcad56ea92e13289a43b2b9629651e68d8fe608
dickyHandoko <dicky@gmail.com>
2016-04-20 14:45:53 +0800

    adding ping web service funct

--- web/index.php
+++ web/index.php
@@ -22,6 +22,12 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+$app->get('/ping ', function() use($app) {
+  $data = array( 'alive' => 'true');
+   //$response->headers->set('Content-Type', 'application/json');
+	//return $response;
+	return json_encode($data);
+});
 
 $app->get('/minesweeper ', function() use($app) {
   return $app['twig']->render('minesweeper.twig');
